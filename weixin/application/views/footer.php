4f7af99746b1e38b7a7ea65614150652644851f2
smileoto <smileoto@qq.com>
2014-11-15 14:32:57 +0800

    百度统计

--- weixin/application/views/footer.php
+++ weixin/application/views/footer.php
@@ -1,6 +1,10 @@
-                <div style=" display:none"><img src="http://120.24.77.31/xuexintong/shareimg/img.php?jpg"></div>		
+        <div style=" display:none"><img src="http://120.24.77.31/xuexintong/shareimg/img.php?jpg"></div>		
 		<footer class="foot">
         	<div>
 			<?php echo $content?>
             </div>
 		</footer>
+        <script type="text/javascript">
+		var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+		document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F2c6a59a86f94b1f2a6897938fd70451d' type='text/javascript'%3E%3C/script%3E"));
+		</script>
